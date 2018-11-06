7dc03f66580e86de7dfd31a3d8b8347156cc1558
pipelineascodecourse <committer@example.com>
2018-11-06 22:09:36 +0800

    updating number of test cases

--- src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
+++ src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
@@ -18,15 +18,15 @@ public class CalculatorTest {
 //		
 //	}
 	
-	@Test
-	public void testMinus()
-	{
-	
-		Calculator calculator = new Calculator();
-		int result = calculator.minus(4, 1);
-		assertEquals(3, result);
-		
-	}
+//	@Test
+//	public void testMinus()
+//	{
+//	
+//		Calculator calculator = new Calculator();
+//		int result = calculator.minus(4, 1);
+//		assertEquals(3, result);
+//		
+//	}
 	
 	@Test
 	public void testMultiply()
