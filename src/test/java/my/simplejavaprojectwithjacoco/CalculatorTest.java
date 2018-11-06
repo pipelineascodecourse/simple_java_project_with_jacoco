11e10c5b586c140f8caee94ef1d3d87c8a2218de
pipelineascodecourse <benjichung@yahoo.com>
2018-11-06 21:15:25 +0800

    commenting out test cases

--- src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
+++ src/test/java/my/simplejavaprojectwithjacoco/CalculatorTest.java
@@ -8,15 +8,15 @@ import my.simplejavaprojectwithjacoco.Calculator;
 
 public class CalculatorTest {
 	
-	@Test
-	public void testAdd()
-	{
-	
-		Calculator calculator = new Calculator();
-		int result = calculator.add(1, 2);
-		assertEquals(3, result);
-		
-	}
+//	@Test
+//	public void testAdd()
+//	{
+//	
+//		Calculator calculator = new Calculator();
+//		int result = calculator.add(1, 2);
+//		assertEquals(3, result);
+//		
+//	}
 	
 	@Test
 	public void testMinus()
